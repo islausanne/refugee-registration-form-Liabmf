5babd3b1f478e1ee40f0d2a58c2881b42f9c22e8
Liabmf <lfoulds@islstudent.ch>
2025-11-18 12:36:26 +0100

    bebras

--- registration.py
+++ registration.py
@@ -11,13 +11,38 @@ def menu():
 def register():
     return render_template('register.html')
 
+# Handle form submission (students will add JSON save code here)
+@app.route('/submit', methods=['POST'])
+def submit_form():
+    name = request.form['name']
+    country = request.form['country']
+    age = request.form['age']
+    medical = request.form['medical']
+
+    if os.path.exists('registrations.json'):
+        with open('registrations.json', 'r') as file:
+            data = json.load(file)
+    else:
+        data = []
+    data.append({'name': name, 'country': country, 'age': age, 'medical': medical})
+
+    with open('registrations.json', 'w') as file:
+        json.dump(data, file, indent=2)
+
+    return redirect(url_for('index'))
+
 @app.route('/edit')
 def edit():
     return render_template('edit.html')
 
 @app.route('/view')
-def view():
-    return render_template('view.html')
+def view_registrations():
+    with open('registrations.json', 'r') as file:
+        data = json.load(file)
+    return render_template('view.html', registrations=data)
+
+    return render_template('view.html', registrations=[])
+
 
 
 if __name__ == '__main__':

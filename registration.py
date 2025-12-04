9d75bb31476484bc040a2dad467c2ab28cbbb928
Liabmf <lfoulds@islstudent.ch>
2025-12-04 15:28:39 +0100

    (commit and push for last class and this class) Date: 2/12/25 Lesson number: 5 1. Goal for today (C i)  State what your plan said you should complete during this session. Next session I want to actually finish all the boxes for real this time, i also want to figure out how to properly split screen my menu 2. Work completed (C iii)  Write one or two sentences summarising what you achieved in this session. I did manage to complete what i wanted this time, i added in the rest of the input boxes i needed, i also split my menu into two halves 3. Did you follow your plan? (C iii and C iv) Yes:  Briefly explain how your progress matched your planned steps. Yes I started off with adding all the boxes and making sure that did work. I also added in my menu page and then fixed up the looks of the menu 4. Next step (C i)  List one or two tasks you intend to complete during the next lesson. Finish fixing the looks of the menu, also create my edit page and neaten up my registration page
    +
    
    Date: 4/12/25
    Lesson number: 6
    1. Goal for today (C i)
     State what your plan said you should complete during this session.
    Finish fixing the looks of the menu, also create my edit page and fix up my registration page to make it more accurate to the design.
    2. Work completed (C iii)
     Write one or two sentences summarising what you achieved in this session.
    I just about finished my menu look, also it turns out i didnt need a menu page in the end so no need to create that, i made one last input box, and also made all my boxes and buttons bigger so it would meet my success criteria.
    3. Did you follow your plan? (C iii and C iv)
    Kind of:
     Briefly explain how your progress matched your planned steps.
    Only because I had planned to make my edit page today, but in the end I didn't need an edit page in the first place. I somewhat followed it, only because i fixed up my menu a little and added a title to my registration
    4. Next step (C i)
     List one or two tasks you intend to complete during the next lesson.
    Next class  is when its due, but over the weekend, I will be finishing writing my comments and also just making sure everything is neat and good for my test on Tuesday.

--- registration.py
+++ registration.py
@@ -1,3 +1,6 @@
+"""
+This is a refugee registration form, where refugees can enter in their data and then view their own and other entries
+"""
 from flask import Flask, request, render_template, redirect, url_for
 import json
 import os
@@ -5,16 +8,18 @@ import os
 
 app = Flask(__name__)
 
+#this is an app route, an app route is basically a page on the website, the '/' shows what comes after the slash in the URL
+# (in this case nothing as it's my homepage)
 @app.route('/')
 def menu():
     return render_template('menu.html')
 
-
+#this is another app route, this takes the user to the registration page where the form is
 @app.route('/register')
 def register():
     return render_template('register.html')
 
-# Handle form submission (students will add JSON save code here)
+# This handles form submission, its basically saying to save the users answers until the variables
 @app.route('/submit', methods=['POST'])
 def submit_form():
     name = request.form['name']
@@ -27,18 +32,20 @@ def submit_form():
     number = request.form['number']
     family = request.form['family']
     medical = request.form['medical']
+    anything = request.form['anything']
 
     if os.path.exists('registrations.json'):
         with open('registrations.json', 'r') as file:
             data = json.load(file)
+#and then here is where it appends the data that has just been saved as a variable and saves it as a list under the variable name 'data'
     else:
         data = []
-    data.append({'name': name, 'lname': lname, 'age': age, 'gender': gender, 'dob': dob, 'country': country, 'email': email, 'number': number, family: family, 'medical': medical})
+    data.append({'name': name, 'lname': lname, 'age': age, 'gender': gender, 'dob': dob, 'country': country, 'email': email, 'number': number, family: family, 'medical': medical, anything: anything})
 
     with open('registrations.json', 'w') as file:
         json.dump(data, file, indent=2)
 
-    return redirect(url_for('view_registrations'))
+    return redirect(url_for('menu'))
 
 @app.route('/edit')
 def edit():
